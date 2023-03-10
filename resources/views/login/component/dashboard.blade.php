993b7832a64cb44feed4f1636ad569e998bf5fbb
Dope-Bit <62684521+Darren-Lionardo@users.noreply.github.com>
2023-03-10 15:07:52 +0700

    update dashboard

--- resources/views/login/component/dashboard.blade.php
+++ resources/views/login/component/dashboard.blade.php
@@ -1,5 +1,5 @@
-<div id="content" class="mt-16 h-screen bg-slate-300 ml-16 pt-6 transition-all duration-500 min-h-[100dvh]">
-    <div id="Dashboardlogo" class="h-20 w-200 flex justify-left">
+<div id="content" class="bg-slate-300 ml-16 pt-6 transition-all duration-500 min-h-[100dvh] overflow-y-auto">
+    <div id="Dashboardlogo" class="h-20 w-200 flex justify-left mt-24">
         <div class="h-20 w-20 ml-11 flex justify-center items-center">
             {{-- <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 512 512">
                 <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M80 212v236a16 16 0 0 0 16 16h96V328a24 24 0 0 1 24-24h80a24 24 0 0 1 24 24v136h96a16 16 0 0 0 16-16V212"/>
@@ -11,9 +11,176 @@
             <p class="h-10 font-bold text-2xl ml-4 flex items-center">Dashboard</p>
             <p class="h-10 ml-4 flex items-center">Welcome, Dosen</p>
         </div>
-        
     </div>
 
+    <div class="p-8 mt-6">
+        <div class="bg-white shadow-sm shadow-gray-600 rounded-3xl p-16 w-full">
+            <div class="h-fit">
+                <div class="font-semibold text-xl text-center py-4">
+                    Jadwal Proses Penelitian Internal Half Term
+                </div>
+                <hr />
+                <div class="mt-8 grid grid-cols-2">
+                    <div class="col-span-2 md:col-span-1">
+                        <div class="text-center mb-4 text-slate-500 font-semibold">
+                            <div class="mb-4">Full Term</div>
+                            <hr />
+                        </div>
+                        <div class="relative top-8">
+                            <div class="border-gray-400 border-l-2 p-6 relative">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    28 Januari 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Batas Penerimaan Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    6 - 10 Februari 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Review Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    15 - 17 Februari 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Seminar Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    20 - 24 Februari 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Kontrak Penelitian
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    10 - 14 Juli 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Monitoring dan Evaluasi Pertama - Skema Full Term dan Half Term
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    7 - 11 November 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Monitoring dan Evaluasi Kedua - Skema Full Term dan Half Term
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    30 November 2023
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Pengumpulan Laporan
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-2 md:col-span-1 mt-16 md:mt-0">
+                        <div class="text-center mb-4 text-slate-500 font-semibold">
+                            <div class="mb-4">Half Term</div>
+                            <hr />
+                        </div>
+                        <div class="relative top-8">
+                            <div class="border-gray-400 border-l-2 p-6 relative">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Batas Penerimaan Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Review Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Seminar Proposal
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Kontrak Penelitian
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Monitoring dan Evaluasi Pertama - Skema Full Term dan Half Term
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Monitoring dan Evaluasi Kedua - Skema Full Term dan Half Term
+                                </div>
+                            </div>
+                            <div class="border-gray-400 border-l-2 p-6 relative ">
+                                <div class="absolute -top-[1rem] -left-[0.6rem] w-4 h-4 rounded-full border border-gray-600 bg-white">
+                                </div>
+                                <div class="absolute -top-[1.3rem] font-semibold text-slate-600">
+                                    TBA
+                                </div>
+                                <div class="text-slate-500 mb-6">
+                                    Pengumpulan Laporan
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
